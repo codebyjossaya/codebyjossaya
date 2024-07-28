0b20903e70ee3a8b2176c6b093cd68ef40f44336
Jossaya Camille <jcamille2023@fau.edu>
2024-07-28 03:23:58 -0400

    updated firebase config

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,25 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './home/Home';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2XHCZtjSk65vvQX0nPbXxvlv6yF_LFXU",
+  authDomain: "jcamille-tech.firebaseapp.com",
+  projectId: "jcamille-tech",
+  storageBucket: "jcamille-tech.appspot.com",
+  messagingSenderId: "484292122701",
+  appId: "1:484292122701:web:59b0828f4a61803487e404",
+  measurementId: "G-KP1SLMXH8S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 function App() {
   return (
   <Router>
@@ -13,3 +32,4 @@ function App() {
 }
 
 export default App;
+export { app, analytics };  // Export the app and analytics objects for use in other files
