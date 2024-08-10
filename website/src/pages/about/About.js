e090e58af3929cff5c60b86ea83cbece457bf2dd
Jossaya Camille <jcamille2023@fau.edu>
2024-08-10 01:44:47 -0400

    Forgot to add a scroll animation lol

--- website/src/pages/about/About.js
+++ website/src/pages/about/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import ScrollAnimation from "../../components/scrollanimation/ScrollAnimation";
 
 function About() {
     return (
     <>
+        <ScrollAnimation>
         <div className="banner">
             <h1>About me</h1>
             <p>From a young age, I've always been interested in computers/computer programming. I've provided a short chronicle of my life and career in CS/Engineering below.</p>
         </div>
+        </ScrollAnimation>
+        <ScrollAnimation>
         <div className="banner">
             <h1>~5 years ago</h1>
             <p>About 5 years ago, I first got into coding through web development. At the time, I was learning HTMl, CSS, a little JavaScript, web hosting, and version control.</p>
@@ -14,12 +18,16 @@ function About() {
             <p>I was also interested at the time in hosting with Heroku, SMS messaging with Twilio, and authentication with Firebase.</p>
             <p>Unfortunately, COVID-19 interrupted this period of time so during the pandemic I had not completed much.</p>
         </div>
+        </ScrollAnimation>
+        <ScrollAnimation>
         <div className="banner">
             <h1>~3 years ago</h1>
             <p>Around this time is when I really got into programming again. I took advanced FLVS courses and learned Python, TkinterUI, Matplotlib, and I was becoming familiar with Numpy.</p>
             <p>I aimed to complete a variety of projects, many of which did not come into fruition.</p>
             <p>Most notably, I submitted a graphing calculator in Python for my AP Computer Science Principles final project and tried to start a social chat app.</p>
         </div>
+        </ScrollAnimation>
+        <ScrollAnimation>
         <div className="banner">
             <h1>~1 year ago - today</h1>
             <p>About 1 year ago, I transferred into FAU High School, allowing me to take advanced programming coursework and participate in a broader range of extracurriculars.</p>
@@ -29,6 +37,7 @@ function About() {
             <p>This led me to working towards conducting undergraduate research supervised by Mahmoud Seifallahi, a graduate student in the Biomedical Signal/Image Analysis Lab run by Dr. Behnaz Ghoraani.</p>
             <p>Today, I hope to continue my research, publish, further refine my skills in computer science and engineering, and use technology to make the world better like I was trying to do 5 years ago with my gaming website.</p>
         </div>
+        </ScrollAnimation>
     </>
     )
 }
